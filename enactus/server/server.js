0c46f5e6841549dab2d0ea6e9be6b0ac94966f5f
Arya Vatan-Abadi <arya.vatanabadi@gmail.com>
2020-02-01 11:34:13 -0500

    Created Usage route and model
    Route and model for usage API's created, theres no API's developed yet

--- enactus/server/server.js
+++ enactus/server/server.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const cors = require('cors')
 
+// API Routes
+const UsageRoutes = require('./api/UsageRoute');
+
 const app = express();
 
 
@@ -16,10 +19,15 @@ app.use(cors())
 
 // ======================================================================================
 // Connect to mongoDB
-const mongoURL = 'mongodb+srv://avatanab:mongopower@mongodb-r6m0u.mongodb.net/SE3316Lab5?retryWrites=true&w=majority';
-mongoose.connect(mongoURL, { useNewUrlParser: true ,  useUnifiedTopology: true })
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err));
+const mongoURL = 'mongodb+srv://avatanab:mongopower@mongodb-r6m0u.mongodb.net/Enactus?retryWrites=true&w=majority';
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log('MongoDB Connected...'))
+	.catch(err => console.log(err));
+
+
+// ======================================================================================
+// Use Routes
+app.use('/api/usage', UsageRoutes);
 
 
 // ======================================================================================
