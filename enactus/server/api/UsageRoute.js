f27e33a8f5b4f236f8210e6c2ad89f69790d0003
Arya Vatan-Abadi <arya.vatanabadi@gmail.com>
2020-02-01 16:36:41 -0500

    POST usage ~working

--- enactus/server/api/UsageRoute.js
+++ enactus/server/api/UsageRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 // Models
 const Client = require('../models/Client');
@@ -23,45 +24,41 @@ router.get('/', (req, res) => {
 
 });
 
-// '/api/usage' - GET
+// '/api/usage' - POST
 router.post('/', (req, res) => {
 	console.log("POST request to /api/usage");
+	let clientName = req.body.clientName;
 	console.log(req.body);
-	// let clientID = getClientId(req.body.clientName);
-	// console.log(clientID);
-	let carbonDollar = req.body.carbonDollar;
-	let carbonTon = carbonDollar/20;
 
-    const newUsage = new Usage({
-        clientID: "clientID",
-		month: req.body.month,
-		year: req.body.year,
-		usageDollar: req.body.usageDollar,
-		usageKwh: req.body.usageKwh,
-		carbonDollar: carbonDollar,
-		carbonTon: carbonTon
-    });
+	axios.get('http://localhost:8080/api/client/' + clientName)
+    .then(response => {
+		let clientID = response.data;
+		let carbonDollar = req.body.carbonDollar;
+		let carbonTon = parseFloat(carbonDollar)/20;
 
-	newUsage.save().then(item => {
-		res.json(item);
-		console.log(item);
-	})
-	.catch(err => {
-        res.status(500).json({error: err});
+		const newUsage = new Usage({
+			clientID: clientID,
+			month: req.body.month,
+			year: req.body.year,
+			usageDollar: req.body.usageDollar,
+			usageKwh: req.body.usageKwh,
+			carbonDollar: carbonDollar,
+			carbonTon: carbonTon
+		});
+	
+		newUsage.save().then(item => {
+			console.log(item);
+			res.json(item);
+		})
+		.catch(err => {
+			res.status(500).json({error: err});
+		});
+    })
+    .catch(error => {
+		console.log(error);
+		res.status(500).json({error: err});
     });
 });
-
-
-// ======================================================================================
-// Helper Functions
-function getClientId(name){
-	Client.findOne({name: name})
-	.then(items => {return items;})
-	.catch(err => {
-		console.log(err);
-		return null;
-    });
-}
 
 
 // ======================================================================================
