63bc1ee67de2a0c12f3acf7ea37d275d3b4742c0
Arya Vatan-Abadi <arya.vatanabadi@gmail.com>
2020-02-01 12:42:41 -0500

    GET/POST API's for Energy Providers
    GET - Get all providers
    POST - Post new provider to database

--- enactus/server/api/ClientRoute.js
+++ enactus/server/api/ClientRoute.js
@@ -3,12 +3,21 @@ const router = express.Router();
 
 // User model
 const Client = require('../models/Client');
+/*	ClientSchema = {
+    	name: String,
+		address: String,
+    	postal: String,
+    	city: String,
+    	province: String,
+		country: String
+	}
+*/
 
 // ======================================================================================
 // API's
 // '/api/client' - GET
 router.get('/', (req, res) => {
-    res.send("Client says hi");
+	res.send("Client says hi");
 });
 
 
--- enactus/server/api/EnergyProviderRoute.js
+++ enactus/server/api/EnergyProviderRoute.js
@@ -3,12 +3,53 @@ const router = express.Router();
 
 // User model
 const EnergyProvider = require('../models/EnergyProvider');
+/*	EnergyProviderSchema = {
+		name: String,
+		type: String,
+		ratePerKwh: Number,
+		address: String,
+		postal: String,
+		city: String,
+		province: String,
+		country: String
+	}
+*/
 
 // ======================================================================================
 // API's
-// '/api/usage' - GET
+
+// '/api/provider' - GET - Get all energy providers
 router.get('/', (req, res) => {
-    res.send("Energy provider says hi");
+	console.log("GET request to /api/provider");
+	EnergyProvider.find().then(items => {
+		res.json(items)
+	})
+	.catch(err => {
+        res.status(500).json({error: err});
+    });
+});
+
+// '/api/provider' - POST - Add new provider to table
+router.post('/', (req, res) => {
+	console.log("POST request to /api/provider");
+    const newEnergyProvider = new EnergyProvider({
+        name: req.body.name,
+		type: req.body.type,
+		ratePerKwh: req.body.ratePerKwh,
+		address: req.body.address,
+		postal: req.body.postal,
+		city: req.body.city,
+		province: req.body.province,
+		country: req.body.country
+    });
+
+	newEnergyProvider.save().then(item => {
+		res.json(item);
+		console.log(item);
+	})
+	.catch(err => {
+        res.status(500).json({error: err});
+    });
 });
 
 
